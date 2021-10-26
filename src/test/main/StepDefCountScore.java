bb2908e4fca1274bb0e53dff73405b98f43b9a11
jiajun-deng123 <jiajundeng@cmail.carleton.ca>
2021-10-26 18:01:56 -0400

    STEP COMMIT

--- src/test/main/StepDefCountScore.java
+++ src/test/main/StepDefCountScore.java
@@ -6,5 +6,20 @@ import io.cucumber.java.en.When;
 import junit.framework.TestCase;
 
 public class StepDefCountScore extends TestCase {
-    
+    gameServer gs;
+
+    @Given("Player played all his card")
+    public void player_played_all_his_card() {
+        gs.handcard.get(gs.handindex).clear();
+    }
+
+    @When("System checks if game over")
+    public void system_checks_if_game_over() {
+        gs.checkEnd();
+    }
+
+    @Then("Game End")
+    public void game_End() {
+        assertTrue(gs.isEnd);
+    }
 }
